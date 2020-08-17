b257bf9b5b562ca1c735843de5f93efa2e04b31d
foxten <tenaysiafox@gmail.com>
2020-08-17 15:07:35 -0500

    Updated migrations

--- db/migrate/20200817200110_create_graphs.rb
+++ db/migrate/20200817200110_create_graphs.rb
@@ -1,7 +1,7 @@
 class CreateGraphs < ActiveRecord::Migration[6.0]
   def change
     create_table :graphs do |t|
-      t.string :iimage_url
+      t.string :image_url
       t.string :equation
       t.array :answer_key
 
