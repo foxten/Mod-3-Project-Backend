543390707ecc72ee6261d3022d5cd4996ff9df03
Yann233 <yannxyang@sina.com>
2020-08-17 17:55:29 -0500

    controller basic

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,2 +1,13 @@
 class GraphsController < ApplicationController
+  def index
+    graphs = Graph.all
+    render json: graphs
+  end
+
+  def show
+    graph = Graph.find(params[:id])
+    render json: graph
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+   @user = User.create(user_params)
+   # render json: @user  ????
+  end
+
+  def delete
+    user = User.find_by(id: params[:id])
+    user.delete
+  end
+
+end
+##########################################
+private
+def user_params
+    params.require(:user).permit(:user_name, :email)
 end
