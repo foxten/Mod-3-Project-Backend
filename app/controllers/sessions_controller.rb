ad4c4f2d17e566c3431a522eca06af4106495888
brennank14 <brennank14@gmail.com>
2020-08-17 19:12:13 -0500

    sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,38 @@
-class SessionsController < ApplicationController
+class SessionsController < ApplicationController    
+
+    
+    def new
+        session = Session.new
+    end
+
+    def create
+        session = Session.create(session_params)
+        if session.save?
+            render json: session
+        end
+    end
+    
+    def edit
+        session = Session.find(params[:id])
+    end
+
+    def update
+        session = Session.update(session_params)
+        if session.save?
+            render json: session
+        end
+    end
+    
+    def destroy
+        session = Session.find(params[:id])
+        session.destroy
+        render json: { message: 'game succesfully deleted' }
+    end
+
+    private
+
+    def session_params
+        params.require(:session).permit(:score, :user_id)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Graph.destroy_all
 
 Graph.create(image_url: 'https://imgur.com/9iAX9yK', equation: 'y = 2x + 3', m: 2, b: 3)
